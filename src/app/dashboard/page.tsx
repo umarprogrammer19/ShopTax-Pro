134783805f79671d18fd7074440890d6daff9837
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-25 14:21:59 +0500

    dd

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,8 +8,7 @@ import { db } from "@/lib/firebase"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import {
-    Store,
+import { Store,
     MapPin,Phone,
     User,Plus,LogOut, CheckCircle,
     XCircle,
